5d60e474619b3178ccb9b810719e8b7d38e23fda
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2022-11-30 19:11:29 +0100

    started with configuration

--- cmd/searchd/main.go
+++ cmd/searchd/main.go
@@ -1,4 +1,20 @@
+// Package main implements the daemon of the search service.
 package main
 
+import (
+	"log"
+
+	"github.com/OpenSlides/openslides-search-service/pkg/config"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatalf("error: %v\n", err)
+	}
+}
+
 func main() {
+	cfg, err := config.GetConfig()
+	check(err)
+	_ = cfg
 }
