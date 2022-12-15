2c128b5d1e663cc4b70335d2c985243968174332
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2022-12-15 11:32:41 +0100

    Add MIT license

--- pkg/search/qserver.go
+++ pkg/search/qserver.go
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 Since 2011 Authors of OpenSlides, see https://github.com/OpenSlides/OpenSlides/blob/master/AUTHORS
+//
+// SPDX-License-Identifier: MIT
+
 // Package search implements the searching in a given database.
 package search
 
